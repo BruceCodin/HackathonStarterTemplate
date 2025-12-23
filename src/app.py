87445fdfc94bcd5f0f224291083f8edf00cef0bd
xmashalx <Mashalchr@gmail.com>
2025-12-23 18:42:50 +0000

    rename

---
+++ src/app.py
@@ -0,0 +1,71 @@
+"""Habit tracking api"""
+
+from flask import Flask, request, render_template, redirect, url_for
+from database import (check_and_apply_decay,
+                      get_all_habits_with_tamagochis, has_completed_today,
+                      create_habit, add_completion,
+                      delete_habit,
+                      get_tamagotchi_by_id,
+                      update_tamagotchi_happiness)
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    """
+    route for the home page which decays any tamagochis if need be
+    updates the completed today field of habits 
+    displays all tamagochis
+    """
+
+    # check and apply decay
+    check_and_apply_decay()
+
+    # get all data
+    habits = get_all_habits_with_tamagochis()
+
+    for habit in habits:
+        habit['completed_today'] = has_completed_today(habit['habit_id'])
+
+    return render_template('index.html', habits=habits)
+
+
+@app.route('/habit/create', methods=['POST'])
+def add_habit():
+    # Get form data
+    name = request.form.get('habit_name')
+    description = request.form.get('description')
+    target_frequency = request.form.get('target_frequency')
+    frequency_unit = request.form.get('frequency_unit')
+    tamagotchi_name = request.form.get('animal_name')
+    animal_type = request.form.get('animal_type')
+
+    # Create habit in DB
+    create_habit(name, description, target_frequency,
+                 frequency_unit, tamagotchi_name, animal_type)
+
+    # Redirect back to home
+    return redirect(url_for('home'))
+
+
+@app.route("/habit/<habit_id>/complete", methods=['POST'])
+def complete_a_habit(habit_id):
+    # add a completion to the db
+    add_completion(habit_id)
+    id = get_tamagotchi_by_id(habit_id)['tamagotchi_id']
+    happiness_level = get_tamagotchi_by_id(habit_id)['happiness_level']
+    update_tamagotchi_happiness(id, happiness_level)
+    return redirect(url_for('home'))
+
+
+@app.route("/habit/<habit_id>/delete")
+def del_old_habit(habit_id):
+    delete_habit(habit_id)
+    return redirect(url_for('home'))
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    app.run(debug=True, host="0.0.0.0", port=5000)
